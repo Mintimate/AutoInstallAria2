0292be8ee54ec348cf8fdd7773e1e13022672531
Mintimate <mintimate215@gmail.com>
2025-04-04 00:31:36 +0800

    feat: 优化安装脚本，改进路径处理和容器支持

--- AutoInstallAria2ForLinux.sh
+++ AutoInstallAria2ForLinux.sh
@@ -4,11 +4,9 @@
 # Author: @Mintimate
 # Blog: https://www.mintimate.cn/about
 
-# 环境地址
-shellPath=$(pwd)/aria2Temp
 # 部署的路径
 ProjectPath=${HOME}/aria2Auto
-remkdir -p ${shellPath}
+rm -rf ${ProjectPath} && mkdir -p ${ProjectPath}
 # echo 标准化
 RED='\033[0;31m'    # 红色
 GREEN='\033[0;32m'  # 绿色
@@ -47,6 +45,8 @@ judgeArchitecture(){
     echo -e "${GREEN} 下载远程配置Aria2源码 ${NC}"
     echo -e "${YELLOW} 判断系统架构 ${NC}"
 
+    # 获取系统架构
+    OS=$(uname -m)
     case ${OS} in
     *86)
         SYS_FLAG='i386'
@@ -70,7 +70,7 @@ judgeArchitecture(){
     echo -e "${GREEN} 正在下载( ´▽｀) ${NC}"
     # aria2 预编译包下载地址
     DOWNLOAD_ARIA2_STATIC=https://cnb.cool/flyinbug/aria2-static-build/-/releases/download/v1.37.0-Turbo_20250402/aria2-static-linux-${SYS_FLAG}.tar.gz
-    wget -qO ${shellPath}/Aria2.tar.gz ${DOWNLOAD_ARIA2_STATIC}
+    wget -qO ${ProjectPath}/Aria2.tar.gz ${DOWNLOAD_ARIA2_STATIC}
     if [ $? -eq 0 ]; then
         echo -e "${GREEN} 下载成功 ${NC}"
     else
@@ -84,20 +84,20 @@ setPassword(){
     echo -e "${GREEN}设置Aria2密码 ${NC}"
     echo -e "${RED} (用于远程Aria2认证) ${NC}"
     echo -e "${RED} (直接输入 no 退出并取消配置) ${NC}"
-    read Aria2token
-    if [ ${Aria2token} == "no" ];then
+    read aria2Password
+    if [ ${aria2Password} == "no" ];then
         echo -e "${RED} 取消配置 ${NC}"
         exit
     fi
-    echo ${Aria2token}
 }
 
 touchAria2Conf(){
     echo -e "${GREEN} 解压Aria2文件 ${NC}"
-    tar -xf ${shellPath}/Aria2.tar.gz -C ${shellPath}/Aria2Temp
+    mkdir -p ${ProjectPath}/Aria2Temp
+    tar -xf ${ProjectPath}/Aria2.tar.gz -C ${ProjectPath}/Aria2Temp
     echo -e "${GREEN} 创建Aria2配置文件夹: ${ProjectPath} ${NC}"
     mkdir -p ${ProjectPath}
-    cp ${shellPath}/Aria2Temp/aria2c ${ProjectPath}/aria2c
+    cp ${ProjectPath}/Aria2Temp/aria2c ${ProjectPath}/aria2c
     echo -e "${GREEN} 云端下载 aria2 配置模板 ${NC}"
     wget -qO ${ProjectPath}/aria2.conf.template https://cnb.cool/Mintimate/tool-forge/AutoInstallAria2/-/git/raw/main/template/aria2.conf.template
     wget -qO ${ProjectPath}/deleteAria2.conf https://cnb.cool/Mintimate/tool-forge/AutoInstallAria2/-/git/raw/main/template/deleteAria2.conf
@@ -120,21 +120,17 @@ registerAria2Systemctl(){
     echo -e "${GREEN} 注册Aria2服务 ${NC}"
     mkdir -p ${HOME}/.config/systemd/user
     sed \
-        -e "s|__HOME__|${HOME}|g" \
         -e "s|__Aria2Home__|${ProjectPath}|g" \
         ${ProjectPath}/aria2.service.template > ${HOME}/.config/systemd/user/aria2.service
     echo -e "${GREEN} 服务注册成功 ${NC}"
     echo -e "${GREEN} 服务启动成功 ${NC}"
     if [[ $(systemd-detect-virt) == "container" || $(systemd-detect-virt) == "docker" ]]; then
         echo -e "${YELLOW}当前在容器内，跳过 systemctl 服务创建 ${NC}"
-        echo -e "${GREEN}启动 Nginx ${NC}"
-        sudo ${NGINX_INSTALL_DIR}/sbin/nginx
-        echo -e "${GREEN}后续重载 Nginx，可以使用命令: ${NC}"
-        echo -e "${GREEN}${NGINX_INSTALL_DIR}/sbin/nginx -s reload ${NC}"
+    else
+        systemctl --user daemon-reload
+        systemctl --user enable aria2.service
+        systemctl --user start aria2.service
     fi
-    systemctl --user daemon-reload
-    systemctl --user enable aria2.service
-    systemctl --user start aria2.service
 }
 
 
@@ -148,22 +144,35 @@ printResult(){
     echo -e "${GREEN} ${HOME}/.config/systemd/user/aria2.service ${NC}"
     echo "————————————————————————————————————————"
     echo -e "${RED} Aria2 RPC 启动命令 ${NC}"
-    echo -e "${GREEN} systemctl --user start aria2.service ${NC}"
+    echo -e "${GREEN}非容器: systemctl --user start aria2.service ${NC}"
+    echo -e "${GREEN}容器内: ${ProjectPath}/aria2c --conf-path=${ProjectPath}/aria2.conf ${NC}"
     echo "————————————————————————————————————————"
     echo -e "${RED} Aria2 RPC 文件下载目录 ${NC}"
     echo -e "${GREEN} ${HOME}/Download ${NC}"
     echo "————————————————————————————————————————"
 }
 
+cleanUpWorkspace(){
+    rm -rf ${ProjectPath}/Aria2.tar.gz
+    rm -rf ${ProjectPath}/Aria2Temp    
+    rm -rf ${ProjectPath}/*.template    
+}
+
 # 打印 logo 和提示信息
 printMintimate
 # 设置 RPC 密码
-aria2Password=$(setPassword)
+setPassword
 # 下载Aria2源文件
 judgeArchitecture
 # 配置 Aria2 
 touchAria2Conf
 # 初始化 Aria2
 initAria2Conf ${aria2Password}
+# 注册 Aria2 服务
+registerAria2Systemctl
+# 打印结果
+printResult
+# 清理工作空间
+cleanUpWorkspace
 
 # unlink $0
